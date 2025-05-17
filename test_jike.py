4433c16e016124d8941c0fce526abf713ef778dc
bangbang-visual <21210730153@m.fudan.edu.cn>
2025-05-17 15:14:19 +0800

    更新适配版

---
+++ test_jike.py
@@ -0,0 +1,6 @@
+from transform_jike import extract_jike_content
+
+# 测试URL
+url = "https://web.okjike.com/u/EB1F98AA-28E8-4437-BD04-56CC68312C78/post/67ffa9497cb8c547e2f5a1d8"
+result = extract_jike_content(url)
+print(result)
